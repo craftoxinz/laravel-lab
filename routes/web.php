13c6b05e491c52dbd21007566ac39c89e81fbcfa
craftoxinz <winnestkyouka12@gmail.com>
2026-04-17 23:35:52 +0700

    chore(routes): add temporary /me test route
    Add a simple GET `/me` route using a closure that returns
    a static string for testing purposes.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Define GET route using a closure
+Route::get('/me', function () {
+    return 'Randie Syaeful Azahli';
+});
