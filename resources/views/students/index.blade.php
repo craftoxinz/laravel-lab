607edcfd36c2f1688eb0e5f56cfade4f6a6d2e64
craftoxinz <winnestkyouka12@gmail.com>
2026-05-05 10:00:53 +0700

    feat(view): add student list page

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,1 +1,31 @@
-<p>Show students</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student List</title>
+</head>
+<body>
+    <h1>Student List</h1>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Code</th>
+            <th>Major</th>
+        </tr>
+
+        @php
+            /**
+             * @var \Illuminate\Database\Eloquent\Collection<App\Models\Student> $students
+             */
+        @endphp
+        @foreach ($students as $student)
+            <tr>
+                <td>{{ $student->name }}</td>
+                <td>{{ $student->code }}</td>
+                <td>{{ $student->major }}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
