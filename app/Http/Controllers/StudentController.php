d402aa93a3ccb52cb61ff64f67cfb82d9cacafb5
craftoxinz <winnestkyouka12@gmail.com>
2026-05-05 10:02:29 +0700

    feat(student): add StudentController index method
    - retrieve all students from database
    - pass data to students index view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+
 class StudentController extends Controller
 {
     public function index()
     {
-        return view('students.index');
+        $students = Student::all();
+
+        return view('students.index', compact('students'));
     }
 }
