3fcdaf81ad2e3d61f165167ded0d7b0bcace6b9f
craftoxinz <winnestkyouka12@gmail.com>
2026-05-09 21:32:50 +0700

    feat(student): add student seed data

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('students')->insert([
+            [
+                'name' => 'Mamat',
+                'code' => '240414099',
+                'major' => 'IF',
+            ],
+            [
+                'name' => 'Cancan',
+                'code' => '240414098',
+                'major' => 'IF',
+            ],
+        ]);
+    }
+}
