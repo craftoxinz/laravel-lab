c14849f5793688e820276c9901c41995e2ba11ba
craftoxinz <winnestkyouka12@gmail.com>
2026-05-09 21:15:32 +0700

    feat(layout): allow custom page title in layout component

--- resources/views/components/layouts/page.blade.php
+++ resources/views/components/layouts/page.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $pageTitle ?? config('app.name', 'Laravel') }}</title>
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
